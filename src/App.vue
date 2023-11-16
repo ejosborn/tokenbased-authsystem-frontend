039492ab24255ef0a381d58a085e0d1fa81b24d8
Ellis Osborn <ejosborn@asu.edu>
2023-11-16 13:15:59 -0800

    added buttons to home page of the app

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <Home msg="Welcome to Token-Based Authentication System" />
+  <div id="app">
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Home
   }
 }
 </script>
