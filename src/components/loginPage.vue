64a68aa31664c21f6c4b5e230d9663b0061d10e0
Ellis Osborn <ejosborn@asu.edu>
2023-11-17 12:58:57 -0800

    added input fields for username and password, added css styling

--- src/components/loginPage.vue
+++ src/components/loginPage.vue
@@ -1,6 +1,15 @@
 <template>
-    <div>
+    <div id="header">
         <h1>Welcome to the Login Page</h1>
+        <div class="input-container" id="userInput">
+            <p class="label-user">Username:</p>
+            <input type="text" v-model="usernameInput" />
+        </div>
+        <div class="input-container" id="passInput">
+            <p class="label-pass">Password:</p>
+            <input type="password" v-model="password" />
+        </div>
+        <button id="loginBtn">Log In</button>
     </div>
 </template>
 
@@ -9,3 +18,24 @@ export default {
     //logic
 }
 </script>
+
+<style>
+.input-container {
+    display: flex;
+    align-items: center;
+    margin-left: 43%;
+    padding-bottom: -5px;
+}
+
+input {
+    font-size: 17px;
+}
+
+.label-user {
+    margin-right: 10px;
+}
+
+.label-pass {
+    margin-right: 14px;
+}
+</style>
